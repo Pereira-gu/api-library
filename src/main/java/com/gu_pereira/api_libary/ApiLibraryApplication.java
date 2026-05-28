93092b103024e7006fb8d22ea41cf537580c86bd
Gustavo Dos Santos Pereira <pereira.worker@gmail.com>
2026-05-28 20:09:51 -0300

    feat: Enable JPA Auditing for Livro entity
    - Added @EnableJpaAuditing to ApiLibraryApplication.
    - Introduced dataCriacao and dataAtualizacao fields with @CreatedDate and @LastModifiedDate in Livro entity.
    - Applied @EntityListeners(AuditingEntityListener.class) to Livro for automatic auditing.

--- src/main/java/com/gu_pereira/api_libary/ApiLibraryApplication.java
+++ src/main/java/com/gu_pereira/api_libary/ApiLibraryApplication.java
@@ -2,8 +2,10 @@ package com.gu_pereira.api_libary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ApiLibraryApplication {
 
     public static void main(String[] args) {
